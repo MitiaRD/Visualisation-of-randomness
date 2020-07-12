8f34d53ee36c90564c13df30b16f0c773a1c8bc1
mit9797 <66083535+mit9797@users.noreply.github.com>
2020-07-12 22:36:47 +0100

    Update visualisati0n_0f_rand0mness.py

--- visualisati0n_0f_rand0mness.py
+++ visualisati0n_0f_rand0mness.py
@@ -1,18 +1,20 @@
 from random import randint
 import matplotlib.pyplot as plt
 
+#creating a matrix of values
 image = []
 for row in range(50):
     rows = []
     for column in range(50):
-        r = randint(0, 155)
+        #randomly assigning red green and blue values within the range of 0 - 255
+        r = randint(0, 255)
         g = randint(0, 255)
         b = randint(0, 255)
         pixel = (r, g, b)
         rows.append(pixel)
     image.append(rows)
 
-
+#displaying our newly created random pattern
 plt.imshow(image)
 plt.title('Visualisation of randomness')
 plt.axis('off')
